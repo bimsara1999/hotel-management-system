e238946e401e0086def959f1918751e866b4367b
bimsara1999 <blankanatha@gmail.com>
2024-08-06 21:08:09 +0545

    New one in booking

--- src/main/java/com/phegondev/PhegonHotel/entity/Booking.java
+++ src/main/java/com/phegondev/PhegonHotel/entity/Booking.java
@@ -38,4 +38,8 @@ public class Booking {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
 }
