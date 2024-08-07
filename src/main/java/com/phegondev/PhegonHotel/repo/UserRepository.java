9ca1db4435c90239a6fdfee6d938f106dadea564
bimsara1999 <blankanatha@gmail.com>
2024-08-08 00:20:04 +0545

    Create jwt utils class

--- src/main/java/com/phegondev/PhegonHotel/repo/UserRepository.java
+++ src/main/java/com/phegondev/PhegonHotel/repo/UserRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
 @Repository
-
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsById(String email);
 
-    Optional<User> findById(String email);
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+
+
 }
