a4d858c4e83fabe04af93d6dc63f51974e302f62
bimsara1999 <blankanatha@gmail.com>
2024-08-08 00:40:36 +0545

    Complete cors config

---
+++ src/main/java/com/phegondev/PhegonHotel/security/JWTAuthFilter.java
@@ -0,0 +1,29 @@
+package com.phegondev.PhegonHotel.security;
+
+
+import com.phegondev.PhegonHotel.service.CustomUserDetailsService;
+import com.phegondev.PhegonHotel.utils.JWTUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JWTAuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTUtils jwtUtils;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        
+
+    }
+}
