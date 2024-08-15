570483f5e774bf62b39dce5bd4131dfcc6d0233b
bimsara1999 <blankanatha@gmail.com>
2024-08-16 00:21:40 +0545

    Create Auth Controller

---
+++ src/main/java/com/phegondev/PhegonHotel/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.phegondev.PhegonHotel.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+  //  @Autowired
+  //  private UserService userService;
+}
