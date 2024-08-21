1f2be1e93b9efbc92c3d934a3a89a7a39a7717cf
bimsara1999 <blankanatha@gmail.com>
2024-08-21 17:26:03 +0545

    Update Booking Service

--- src/main/java/com/phegondev/PhegonHotel/service/impl/BookingService.java
+++ src/main/java/com/phegondev/PhegonHotel/service/impl/BookingService.java
@@ -2,11 +2,23 @@ package com.phegondev.PhegonHotel.service.impl;
 
 import com.phegondev.PhegonHotel.dto.Response;
 import com.phegondev.PhegonHotel.entity.Booking;
+import com.phegondev.PhegonHotel.repo.BookingRepository;
+import com.phegondev.PhegonHotel.repo.RoomRepository;
 import com.phegondev.PhegonHotel.service.interfac.IBookingService;
+import com.phegondev.PhegonHotel.service.interfac.IRoomService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookingService implements IBookingService {
+
+    @Autowired
+    private BookingRepository bookingRepository;
+    @Autowired
+    private IRoomService roomService;
+    @Autowired
+    private RoomRepository roomRepository;
+
     @Override
     public Response saveBooking(Long roomId, Long userId, Booking bookingRequest) {
         return null;
