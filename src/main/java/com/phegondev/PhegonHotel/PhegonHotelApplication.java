3163a18f2363fcd04993ca21848d080658b85a2b
bimsara1999 <blankanatha@gmail.com>
2024-08-21 15:15:31 +0545

    Create Room service

--- src/main/java/com/phegondev/PhegonHotel/PhegonHotelApplication.java
+++ src/main/java/com/phegondev/PhegonHotel/PhegonHotelApplication.java
@@ -1,13 +1,20 @@
 package com.phegondev.PhegonHotel;
 
+import com.phegondev.PhegonHotel.service.AwsS3Service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PhegonHotelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhegonHotelApplication.class, args);
+	}
+
+	@Bean
+	public AwsS3Service awsS3Service() {
+		return new AwsS3Service();
 	}
 
 }
