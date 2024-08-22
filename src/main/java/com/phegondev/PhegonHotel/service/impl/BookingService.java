575652d9f542e5cda9a91faa61e1cb4a3b8ce507
bimsara1999 <blankanatha@gmail.com>
2024-08-22 11:38:36 +0545

    Update Booking service

--- src/main/java/com/phegondev/PhegonHotel/service/impl/BookingService.java
+++ src/main/java/com/phegondev/PhegonHotel/service/impl/BookingService.java
@@ -1,13 +1,21 @@
 package com.phegondev.PhegonHotel.service.impl;
 
+import com.phegondev.PhegonHotel.dto.BookingDTO;
 import com.phegondev.PhegonHotel.dto.Response;
 import com.phegondev.PhegonHotel.entity.Booking;
+import com.phegondev.PhegonHotel.entity.Room;
+import com.phegondev.PhegonHotel.entity.User;
+import com.phegondev.PhegonHotel.exception.OurException;
 import com.phegondev.PhegonHotel.repo.BookingRepository;
 import com.phegondev.PhegonHotel.repo.RoomRepository;
+import com.phegondev.PhegonHotel.repo.UserRepository;
 import com.phegondev.PhegonHotel.service.interfac.IBookingService;
 import com.phegondev.PhegonHotel.service.interfac.IRoomService;
+import com.phegondev.PhegonHotel.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookingService implements IBookingService {
@@ -18,15 +26,97 @@ public class BookingService implements IBookingService {
     private IRoomService roomService;
     @Autowired
     private RoomRepository roomRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public Response saveBooking(Long roomId, Long userId, Booking bookingRequest) {
-        return null;
+
+        Response response = new Response();
+
+        try{
+            if(bookingRequest.getCheckOutDate().isBefore(bookingRequest.getCheckInDate())){
+                throw  new IllegalArgumentException("Check in Date must come after checkout date");
+            }
+            Room room = roomRepository.findById(roomId).orElseThrow(()-> new OurException("Room not Found"));
+            User user = userRepository.findById(userId).orElseThrow(()-> new OurException("User not Found"));
+
+            List<Booking> existingBookings = room.getBookings();
+
+            if (!roomIsAvailable(bookingRequest, existingBookings)) {
+                throw new OurException("Room not Available for selected date range");
+            }
+
+            bookingRequest.setRoom(room);
+            bookingRequest.setUser(user);
+            String bookingConfirmationCode = Utils.generateRandomConfirmationCode(10);
+            bookingRequest.setBookingConfirmationCode(bookingConfirmationCode);
+            bookingRepository.save(bookingRequest);
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setBookingConfirmationCode(bookingConfirmationCode);
+
+        } catch (OurException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error Saving a booking: " + e.getMessage());
+
+        }
+
+
+        return response;
+    }
+
+    private boolean roomIsAvailable(Booking bookingRequest, List<Booking> existingBookings) {
+        return existingBookings.stream()
+                .noneMatch(existingBooking ->
+                        bookingRequest.getCheckInDate().equals(existingBooking.getCheckInDate())
+                                || bookingRequest.getCheckOutDate().isBefore(existingBooking.getCheckOutDate())
+                                || (bookingRequest.getCheckInDate().isAfter(existingBooking.getCheckInDate())
+                                && bookingRequest.getCheckInDate().isBefore(existingBooking.getCheckOutDate()))
+                                || (bookingRequest.getCheckInDate().isBefore(existingBooking.getCheckInDate())
+
+                                && bookingRequest.getCheckOutDate().equals(existingBooking.getCheckOutDate()))
+                                || (bookingRequest.getCheckInDate().isBefore(existingBooking.getCheckInDate())
+
+                                && bookingRequest.getCheckOutDate().isAfter(existingBooking.getCheckOutDate()))
+
+                                || (bookingRequest.getCheckInDate().equals(existingBooking.getCheckOutDate())
+                                && bookingRequest.getCheckOutDate().equals(existingBooking.getCheckInDate()))
+
+                                || (bookingRequest.getCheckInDate().equals(existingBooking.getCheckOutDate())
+                                && bookingRequest.getCheckOutDate().equals(bookingRequest.getCheckInDate()))
+                );
     }
 
     @Override
     public Response findBookingByConfirmationCode(String confirmationCode) {
-        return null;
+        Response response = new Response();
+
+        try{
+          Booking booking = bookingRepository.findByBookingConfirmationCode(confirmationCode).orElseThrow(()-> new OurException("Booking Not Found"));
+            BookingDTO bookingDTO = Utils.mapBookingEntityToBookingDTO(booking)
+
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setBookingConfirmationCode(bookingConfirmationCode);
+
+        } catch (OurException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error Saving a booking: " + e.getMessage());
+
+        }
+
+
+        return response;
+    }
     }
 
     @Override
